522e0e5469c33f977c7a7264334c928983884c04
lwawyou <lwawyou@gmail.com>
2017-10-09 18:35:07 +0800

    Animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-document.onload(function(){
-    $('.carousel').carousel();
-});
+window.onload = function(){
+    console.log("Initializing custom javascript.");
+    window.onscroll = function (e) {  
+    // called when the window is scrolled.  
+        console.log("Event Scroll ", e.scrollTop);
+        $(".animate-right").addClass("in-view");
+    } 
+};
